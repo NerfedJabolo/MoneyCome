5c34b7e93dd68253fa2c63a92a6fae5fdba5f8be
NerfedJabolo <ezdlprofit1@gmail.com>
2023-03-04 22:48:23 +0200

    made a goofy ahh react clone without node

--- components/app.js
+++ components/app.js
@@ -1,1 +1,53 @@
+function includeHTML() {
+  var z, i, elmnt, files;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName('*');
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain attribute:*/
+    files = elmnt.getAttribute('include-html');
+    if (files) {
+      files = files.split(',');
+      /* Chain the requests in sequence using a callback function: */
+      loadFilesInSequence(files, elmnt, function () {
+        /* Remove the attribute: */
+        elmnt.removeAttribute('include-html');
+        /* Call the function again to include any remaining files: */
+        includeHTML();
+      });
+      /* Exit the function: */
+      return;
+    }
+  }
+}
 
+function loadFilesInSequence(files, elmnt, callback) {
+  var i = 0;
+
+  function next() {
+    if (i < files.length) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.insertAdjacentHTML('afterend', this.responseText);
+            i++;
+            next();
+          } else if (this.status == 404) {
+            elmnt.insertAdjacentHTML('afterend', 'Page not found.');
+            i++;
+            next();
+          }
+        }
+      };
+      xhttp.open('GET', files[i], true);
+      xhttp.send();
+    } else {
+      callback();
+    }
+  }
+
+  next();
+}
+
+includeHTML();
